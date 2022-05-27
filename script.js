644b4606666f0c2dcfa8dbd1d212a7d42c19440a
Le Duck <95136800+duck123acb@users.noreply.github.com>
2022-05-27 08:35:16 -0600

    added website

---
+++ script.js
@@ -0,0 +1,21 @@
+const btn = document.getElementById("more");
+const display = document.getElementById("amt-display");
+const amtAdd = document.getElementById("amt-add");
+let amtToAdd = 1;
+let score = 0;
+
+btn.addEventListener("click", function() {
+	score+=amtToAdd;
+	display.innerText = "VBUCKS amt: " + score;
+});
+
+amtAdd.addEventListener('keydown', function (e) {
+	if (!e) e = window.event;
+	switch (e.key) {
+		case "Enter":
+			amtToAdd = parseInt(amtAdd.value);
+			break;
+		default:
+			return;
+	}
+});
