fc8f25aa14e84d558bc01486bef85291f848124f
Le Duck <95136800+duck123acb@users.noreply.github.com>
2022-05-31 08:43:14 -0600

    added really random codes

--- script.js
+++ script.js
@@ -2,16 +2,32 @@ const btn = document.getElementById("more");
 const display = document.getElementById("amt-display");
 const vbuk = document.getElementById("vbuk-code")
 const amtAdd = document.getElementById("amt-add");
-const codeBtn = document.getElementById("code");
-const vbukCode = document.getElementById("vbuk-code");
-const codes = []
+const chars = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 let amtToAdd = 1;
 let score = 0;
+let code = "";
+let currentChar = 0;
+
+
+function getCode(){
+	code = "";
+	for(let i = 0; i < 19; i++){
+		if (i == 4 | i == 9 |i == 14){
+			code += "-";
+		}
+		
+		else{
+			currentChar = Math.floor(Math.random() * chars.length);
+			code += chars[currentChar]
+		}
+	}
+}
 
 btn.addEventListener("click", function() {
 	score+=amtToAdd;
+	getCode();
 	display.innerText = "VBUCKS amt: " + score;
-	vbuk.innerText = "Your code is: 93MO 67MZ 7U99 SKMQ";
+	vbuk.innerText  = "Your code is: " + code;
 });
 
 amtAdd.addEventListener('keydown', function (e) {
@@ -23,8 +39,4 @@ amtAdd.addEventListener('keydown', function (e) {
 		default:
 			return;
 	}
-});
-
-codeBtn.addEventListener("click", function() {
-	vbukCode.innerText = "Your code is: " + codes[Math.floor(Math.random() * codes.length)];
 });
