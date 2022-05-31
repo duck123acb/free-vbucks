727dec352e80d3b45148d6e925aa6c9ddb28ad28
hephaestushex <81438978+hephaestushex@users.noreply.github.com>
2022-05-31 07:04:00 -0600

    added random vbuck codes

--- script.js
+++ script.js
@@ -2,13 +2,22 @@ const btn = document.getElementById("more");
 const display = document.getElementById("amt-display");
 const vbuk = document.getElementById("vbuk-code")
 const amtAdd = document.getElementById("amt-add");
+const chars = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10"];
 let amtToAdd = 1;
 let score = 0;
+let code = "";
+let currentChar = 0;
+
+
+for(let i = 0; i < 16; i++){
+	currentChar = Math.floor(Math.random() * chars.length);
+	code += chars[currentChar]
+}
 
 btn.addEventListener("click", function() {
 	score+=amtToAdd;
 	display.innerText = "VBUCKS amt: " + score;
-	vbuk.innerText = "Your code is: 93MO 67MZ 7U99 SKMQ";
+	vbuk.innerText  = "Your code is:" + code;
 });
 
 amtAdd.addEventListener('keydown', function (e) {
