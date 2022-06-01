0d21963a51fa7367c73afd8a3d9250152c050bd5
hephaestushex <81438978+hephaestushex@users.noreply.github.com>
2022-05-31 19:57:46 -0600

    change message

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 const btn = document.getElementById("more");
-const codebtn = document.getElementById("code");
+const codeBtn = document.getElementById("code");
 const display = document.getElementById("amt-display");
-const help = document.getElementById("help-text")
-const vbuk = document.getElementById("vbuk-code")
+const help = document.getElementById("help");
+const helpText = document.getElementById("help-text");
+const vbuk = document.getElementById("vbuk-code");
 const amtAdd = document.getElementById("amt-add");
 const chars = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 let amtToAdd = 1;
@@ -11,28 +12,28 @@ let code = "";
 let currentChar = 0;
 let toggle = false;
 
-function getCode(){
+function getCode() {
 	code = "";
-	for(let i = 0; i < 19; i++){
-		if (i == 4 | i == 9 |i == 14){
+	for (let i = 0; i < 19; i++) {
+		if (i == 4 | i == 9 | i == 14) {
 			code += "-";
 		}
-		
-		else{
+
+		else {
 			currentChar = Math.floor(Math.random() * chars.length);
 			code += chars[currentChar]
 		}
 	}
 }
 
-btn.addEventListener("click", function() {
-	score+=amtToAdd;
+btn.addEventListener("click", function () {
+	score += amtToAdd;
 	display.innerText = "VBUCKS amt: " + score;
 });
 
-codebtn.addEventListener("click", function(){
+codeBtn.addEventListener("click", function () {
 	getCode();
-	vbuk.innerText  = "Your code is: " + code;
+	vbuk.innerText = "Your code is: " + code;
 });
 
 amtAdd.addEventListener('keydown', function (e) {
@@ -46,8 +47,8 @@ amtAdd.addEventListener('keydown', function (e) {
 	}
 });
 
-help.addEventListener("click", function(){
-	toggle=!toggle;
-	if (toggle) // show
-	else //hide
+help.addEventListener("click", function () {
+	toggle = !toggle;
+	if (toggle) helpText.style.display = "block";
+	else helpText.style.display = "none";
 });
